312a1b4ccbc459c7e814a43e0177e357ac8d86b4
Amoatey <emmanuel.amoatey@amalitech.org>
2023-07-04 10:47:03 +0000

    style admin registration/photo upload

--- client/src/components/AdminRegistration/index.tsx
+++ client/src/components/AdminRegistration/index.tsx
@@ -1,17 +1,38 @@
-import { Styledcontainer } from "./styles"
-
+import { Styledcontainer } from "./styles";
+import Logo from "../../assets/Icons/logo.png";
+import photo from "../../assets/Images/Frame a.png";
+import { AiOutlineCamera } from "react-icons/ai";
 
 export const Admin = () => {
-
-
-
-
   return (
     <Styledcontainer>
-      <div className="container">
-        <div className="registration">
+      <div className="container-body">
+        <div className="container">
+          <img className="kanbanlogo" src={Logo} alt="logo" />
+          <div className="registration">
+            <div className="register-container">
+              <h1>Registration</h1>
+              <p>
+                Pleae fill out the required fields below to create your account.
+                We look forward to having yo join our community
+              </p>
+              <div className="upload-photo">
+                <div className="admin-photo">
+                  <img
+                    className="admin-photo"
+                    src={photo}
+                    alt="photo display"
+                  />
+                </div>
+                <button>
+                  <AiOutlineCamera className="uploadIcon" />
+                  <p>Upload picture</p>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Styledcontainer>
   );
-}
+};
--- client/src/components/AdminRegistration/styles.ts
+++ client/src/components/AdminRegistration/styles.ts
@@ -2,12 +2,98 @@ import styled from "styled-components"
 
 
 export const Styledcontainer = styled.div`
-  .container {
+  .container-body {
     width: 100%;
-    height: 100vh;
-    overflow-x: hidden;
+    height: 100%;
     background-color: #e4ebfa;
+    overflow-x: hidden;
+    display: flex;
+    justify-content: center;
+    /* align-items: center; */
+    /* padding: 32px; */
   }
 
- 
+  .kanbanlogo {
+    width: 296px;
+    height: 49px;
+    margin: 163px 0 90px 0;
+    /* margin-left: 364px; */
+    /* margin-top: 163px; */
+    /* margin: auto; */
+    /* margin-bottom: 90px; */
+  }
+
+  .registration {
+    width: 712px;
+    height: 558px;
+    background-color: #fff;
+    border-radius: 8px;
+    margin-bottom: 164px;
+    margin: 0 0 164px 0;
+
+    /* margin-top: 90px;
+    margin-left: 364px; */
+    /* padding: 648px x 494px */
+
+    .register-container {
+      width: 648px;
+      height: 494px;
+      padding: 32px;
+      /* display: flex; */
+      /* flex-direction: column; */
+      /* justify-content: center; */
+    }
+    .register-container h1 {
+      font-size: 18px;
+      color: #20212c;
+      font-weight: 700;
+    }
+
+    .register-container p {
+      font-size: 13px;
+      font-weight: 500;
+      line-height: 23px;
+      color: #828fa3;
+    }
+
+    .upload-photo {
+      width: 290px;
+      height: 107px;
+      margin: auto;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .admin-photo {
+      width: 107px;
+      height: 107px;
+      border-radius: 50%;
+    }
+
+    .upload-photo button {
+      border-radius: 100px;
+      border: 1px solid rgba(130, 143, 163, 0.25);
+      background-color: #635fc7;
+      width: 151px;
+      height: 40px;
+      padding: 0px 16px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      color: white;
+      
+    }
+    
+    .uploadIcon {
+      width: 24px;
+      height: 24px;
+    }
+    
+    .upload-photo button p {
+      font-size: 13px;
+      font-weight: 500;
+    }
+  }
 `;
