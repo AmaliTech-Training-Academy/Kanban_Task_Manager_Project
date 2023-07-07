1776cc84ac6d1e9b805e4f9a1807efc96f659435
Amoatey <emmanuel.amoatey@amalitech.org>
2023-07-07 22:12:21 +0000

    user/admin login page

---
+++ client/src/admin-user-login/index.tsx
@@ -0,0 +1,26 @@
+import { StyledAdminLoginContainer } from "./styles";
+import Logo from "../assets/Icons/logo.png";
+import { LoginFooter } from "../components/LoginFooter";
+import { LoginForm } from "../components/LoginForm";
+
+export const AdminLogin = () => {
+  return (
+    <StyledAdminLoginContainer>
+      <div className="container-body">
+        <div className="container">
+          <div className="login">
+            <div className="login-container">
+              <img className="kanbanlogo" src={Logo} alt="logo" />
+              <h1>Log in</h1>
+              <p className="text">
+                "Please enter your login details below to access you account."
+              </p>
+              <LoginForm />
+              <LoginFooter />
+            </div>
+          </div>
+        </div>
+      </div>
+    </StyledAdminLoginContainer>
+  );
+};
---
+++ client/src/components/LoginFooter/index.tsx
@@ -0,0 +1,13 @@
+import { StyledLoginFooter } from "./styles";
+
+export const LoginFooter = () => {
+  return (
+    <div>
+      <StyledLoginFooter>
+        <footer>
+          Don't have an account? Register <a href="http:// ">Register</a>
+        </footer>
+      </StyledLoginFooter>
+    </div>
+  );
+};
