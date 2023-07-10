735660aec66c3420270602b6ecda4023aaadf679
Amoatey <emmanuel.amoatey@amalitech.org>
2023-07-10 09:44:22 +0000

    completion

--- client/src/forgot-password/index.tsx
+++ client/src/forgot-password/index.tsx
@@ -1,6 +1,6 @@
 import { StyledForgotPasswordContainer } from "./styles";
 import Logo from "../assets/Icons/logo.png";
-import { ForgotPasswordFooter  } from "../components/ForgotPasswordFooter";
+import { ForgotPasswordFooter } from "../components/ForgotPasswordFooter";
 import { ForgotPasswordForm } from "../components/ForgotPasswordForm";
 
 export const ForgotPassword = () => {
@@ -13,9 +13,9 @@ export const ForgotPassword = () => {
               <img className="kanbanlogo" src={Logo} alt="logo" />
               <h1>Forgot password</h1>
               <p className="text">
-                Enter your email address and click on the submit button below to recieve an email instruction on how to reset your password
+                Enter your email address and click on the submit button below to
+                recieve an email instruction on how to reset your password
               </p>
-
               <ForgotPasswordForm />
               <ForgotPasswordFooter />
             </div>
