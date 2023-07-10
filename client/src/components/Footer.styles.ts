d8eada8d129ca761ebe6f4aa84bab2b7b27e2b85
Francisca <francisca.amponsah@amalitech.org>
2023-07-10 01:23:45 +0000

    reorganize CSS styles in the footer component

--- client/src/components/Footer.styles.ts
+++ client/src/components/Footer.styles.ts
@@ -1,46 +1,86 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Stylecontainer = styled.div`
-
-.footer{
-    /* margin-top: 350px; */
+  .footer {
     padding: 2rem 5rem;
-    
-}
-.footerr{
+    display: flex;
+    justify-content: space-between;
+  }
+  .footerr {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    /* background-color: red; */
-}
-a{
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 211px;
+  }
+  a {
     text-decoration: none;
     color: #000;
-}
-.social{
-    margin-left: 35rem;
-}
-.input{
-    border-radius: 20px;
-    /* border: none; */
+  }
+  .footerr h3 {
+    color: #20212c;
+    font-family: Plus Jakarta Sans;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+  .footerr p {
     color: #000;
-font-family: Plus Jakarta Sans;
-font-size: 18px;
-font-style: normal;
-font-weight: 700;
-line-height: normal;
-opacity: 0.30000001192092896;
-}
-.subscribe{
+    font-family: Plus Jakarta Sans;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    opacity: 0.6000000238418579;
+  }
+
+  .enterFold h3 {
+    color: #20212c;
+    font-family: Plus Jakarta Sans;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+  .enterFold p {
+    display: flex;
+    width: 310px;
+    flex-direction: column;
+    color: #000;
+    font-family: Plus Jakarta Sans;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    opacity: 0.6000000238418579;
+  }
+  .icons {
+    text-align: center;
+  }
+  .input {
+    border-radius: 20px;
+    color: #000;
+    font-family: Plus Jakarta Sans;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    opacity: 0.30000001192092896;
+    padding: 8px;
+    gap: 8px;
+    text-align: center;
+  }
+  .subscribe {
     width: 128px;
-height: 54px;
-padding: 8px;
-justify-content: center;
-align-items: center;
-color: #fff;
-gap: 8px;
-flex-shrink: 0;
-border-radius: 100px;
-background: #635FC7;
-box-shadow: 0px 4px 9px 0px rgba(0, 0, 0, 0.25);
-}
-`
+    height: 54px;
+    padding: 8px;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    gap: 8px;
+    flex-shrink: 0;
+    border-radius: 100px;
+    background: #635fc7;
+    box-shadow: 0px 4px 9px 0px rgba(0, 0, 0, 0.25);
+    margin-left: 15px;
+  }
+`;
