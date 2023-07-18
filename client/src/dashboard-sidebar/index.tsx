18c6dc71067f18a052968d714488532d98881d80
Amoatey <emmanuel.amoatey@amalitech.org>
2023-07-18 20:15:57 +0000

    restyle nav bar|clear some errors

--- client/src/dashboard-sidebar/index.tsx
+++ client/src/dashboard-sidebar/index.tsx
@@ -1,23 +1,21 @@
 import * as React from "react";
 import { useState } from "react";
-import {
-  StyledDashSideBar,
-  StyledDashSideNav,
-} from "../dashboard-sidebar/styles";
-import Logo from "../assets/Icons/logo.png";
-import DashLogo from "../assets/Icons/dashboard icon.svg"
-import TaskLogo from "../assets/Icons/task icon.svg";
-import TeamLogo from "../assets/Icons/team icon.svg";
-import CalendarLogo from "../assets/Icons/calender icon.svg";
-import MessagesLogo from "../assets/Icons/Messages icon.svg";
-import SettingsLogo from "../assets/Icons/Settings.svg";
-import LogoutLogo from "../assets/Icons/logout icon.svg";
-import LightIcon from "../assets/Icons/light mode icon.svg";
-import DarkIcon from "../assets/Icons/dark mode icon.svg";
+import {StyledDashSideBar, StyledNavContainer, LeftContainer,RightContainer, NavbarInnerContainer} from "../dashboard-sidebar/styles";
+import Kanbanlogo from "../assets/Icons/logo.png";
+// import DashLogo from "../assets/Icons/fluent_board-split-24-regular.svg"
+// import TaskLogo from "../assets/Icons/task icon.svg";
+// import TeamLogo from "../assets/Icons/team icon.svg";
+// import CalendarLogo from "../assets/Icons/calender icon.svg";
+// import MessagesLogo from "../assets/Icons/Messages icon.svg";
+// import SettingsLogo from "../assets/Icons/Settings.svg";
+// import LogoutLogo from "../assets/Icons/logout icon.svg";
+// import LightIcon from "../assets/Icons/light mode icon.svg";
+// import DarkIcon from "../assets/Icons/dark mode icon.svg";
 import searchIcon from "../assets/Icons/searchIcon.svg"
 import notiIcon from "../assets/Icons/Notification icon.svg"
-import ProfileIcon from "../assets/Images/blank image.svg";
-import hidesidebarIcon from "../assets/Icons/Hide side bar icon.svg"
+import ProfileImg from "../assets/Images/blank image.svg";
+// import hidesidebarIcon from "../assets/Icons/Hide side bar icon.svg";
+
 
 
 
@@ -30,103 +28,144 @@ import hidesidebarIcon from "../assets/Icons/Hide side bar icon.svg"
 
 export const DashBoardSideBar = () => {
 
-  const ToggleButton = () => {
-  const [isToggled, setIsToggled] = useState(false);
+// function Sidebar () {
+//   const [sidebarVisible, setSidebarVisible] = useState(false);
 
-  const handleToggle = () => {
-    setIsToggled(!isToggled);
-  };
-}
+//   const showSidebar = () => setSidebarVisible(!sidebarVisible);
 
+// }
+
+// const [isOpenSideBar, setIsOpenSideBar ] = useState(true);
+
+// const toggleSideBar = () => {
+//   const sideBar = document.querySelector(".SideBar")
+//   sideBar.style.width ="0px !important"
+
+//   setIsOpenSideBar((curr) => !curr 
+//   )
+// }
+
+            
   
-
-
 
   return (
     <>
-      <StyledDashSideBar>
-        <div className="SideBar">
-          <img className="SideBarLogo" src={Logo} alt="logo" />
-          <div className="SideBarContent">
-            <p className="All-Boards">All BOARDS</p>
+      {/* {isOpenSideBar && (
+        <StyledDashSideBar>
+          <div className="SideBar">
+            <div className="SideBarContent">
+              <p className="All-Boards">All BOARDS</p>
 
-            <div className="menu MenuItems">
-              <div className="Dashboard">
-                <div className="item1">
-                  <img src={DashLogo} alt="" />
-                  Dashboard
+              <div className="menu MenuItems">
+                <div className="Dashboard">
+                  <div className="item1">
+                    <img src={Logo} alt="" />
+                    Dashboard
+                  </div>
+                </div>
+                <div className="Taskslist">
+                  <div className="item2">
+                    <img src={TaskLogo} alt="" />
+                    Tasks list
+                  </div>
+                </div>
+                <div className="Teams">
+                  <div className="item3">
+                    {" "}
+                    <img src={TeamLogo} alt="" />
+                    Teams
+                  </div>
+                </div>
+                <div className="Calendar">
+                  <div className="item4">
+                    <img src={CalendarLogo} alt="" />
+                    Calender
+                  </div>
+                </div>
+                <div className="Messages">
+                  <div className="item5">
+                    <img src={MessagesLogo} alt="" />
+                    Messages
+                    <div className="MessageCounter"></div>
+                  </div>
+                </div>
+                <div className="Settings">
+                  <div className="item6">
+                    <img src={SettingsLogo} alt="" />
+                    Settings
+                  </div>
                 </div>
               </div>
-              <div className="Taskslist">
-                <div className="item2">
-                  <img src={TaskLogo} alt="" />
-                  Tasks list
-                </div>
+              <div className="logout">
+                <img className="logout-logo" src={LogoutLogo} alt="" />
+                Logout
               </div>
-              <div className="Teams">
-                <div className="item3">
-                  {" "}
-                  <img src={TeamLogo} alt="" />
-                  Teams
-                </div>
-              </div>
-              <div className="Calendar">
-                <div className="item4">
-                  <img src={CalendarLogo} alt="" />
-                  Calender
-                </div>
-              </div>
-              <div className="Messages">
-                <div className="item5">
-                  <img src={MessagesLogo} alt="" />
-                  Messages
-                <div className="MessageCounter"></div>
-                </div>
-              </div>
-              <div className="Settings">
-                <div className="item6">
-                  <img src={SettingsLogo} alt="" />
-                  Settings
-                </div>
-              </div>
-            </div>
-            <div className="logout">
-              <img className="logout-logo" src={LogoutLogo} alt="" />
-              Logout
-            </div>
-            <div className="Theme">
-              <div className="ThemeContent">
-                <img src={LightIcon} alt="" />
-                <div className="ThemeToggle-Btn "></div>
 
-                <img src={DarkIcon} alt="" />
+              <div className="Theme">
+                <div className="ThemeContent">
+                  <img src={LightIcon} alt="Light" />
+                  <div className="ThemeToggle-Btn"></div>
+                  <img src={DarkIcon} alt="Dark" />
+                </div>
               </div>
-            </div>
-            <div className="HideSideBar">
-              <div className="HideSideBarText">
+              <div className="HideSideBar">
+              <button
+                className= {isOpenSideBar ? "ShowSideBar" :   "HideSideBarText" }
+                onClick={() => setIsOpenSideBar((curr) => !curr)}
+              >
                 <img src={hidesidebarIcon} alt="" />
                 Hide Sidebar
+              </button>
+              {isOpenSideBar ? (
+                <button className="HideSideBarText" onClick={toggleSideBar}>
+                  <img src={hidesidebarIcon} alt="" />
+                  Hide Sidebar
+                </button>
+              ) : (
+                <button className="ShowSideBar" onClick={toggleSideBar}>
+                  <img src={hidesidebarIcon} alt="" />
+                </button>
+              )}
               </div>
             </div>
           </div>
-        </div>
-      </StyledDashSideBar>
-      <StyledDashSideNav>
-        <div className="NavBar">
-          <div className="navtitle"><h1>Task</h1></div>
-          <div className="SeacrchBtn-AddNewTask">
-            <div className="SearchBtn">
-              <img className="searchIcon" src={searchIcon} alt="" />
-              <input type="text" placeholder=" Search" />
+        </StyledDashSideBar>
+      )} */}
+      <StyledNavContainer>
+        <NavbarInnerContainer>
+          <LeftContainer>
+            <img className="Kanbanlogo" src={Kanbanlogo} alt="" />
+          </LeftContainer>
+          <RightContainer>
+            <div className="Navtitle">DashBoard</div>
+
+            <div className="SearchBtn-AddNewTask">
+              <div className="SearchBtn">
+                <img className="searchIcon" src={searchIcon} alt="" />
+                <input type="text" placeholder=" Search" />
+              </div>
+              <button className="AddNewTask-Btn">+ Add New Task</button>
             </div>
+            <div className="notification">
+              <img className="noti" src={notiIcon} alt="" />
+              <img className="profile" src={ProfileImg} alt="" />
+            </div>
+          </RightContainer>
+        </NavbarInnerContainer>
+      </StyledNavContainer>
+
+      <div className="NavBar">
+        {/* <div className="navtitle">
+            <h1>Task</h1>
+          </div>
+          <div className="SeacrchBtn-AddNewTask">
+           
             <button className="AddNewTask-Btn">+Add New Task</button>
           </div>
           <div className="notification">
             <img className="noti" src={notiIcon} alt="" />
-            <img className = "profile" src={ProfileIcon} alt="" />
-          </div>
-        </div>
-      </StyledDashSideNav>
+          </div> */}
+      </div>
     </>
   );
 };
--- client/src/dashboard-sidebar/styles.ts
+++ client/src/dashboard-sidebar/styles.ts
@@ -1,21 +1,11 @@
 import styled from "styled-components";
 
-export const StyledDashSideBar = styled.div`
-  .container-body {
-    width: auto;
-    /* height: 100vh; */
-    background-color: #e4ebfa;
-    overflow-x: hidden;
-    /* display: flex; */
-    /* justify-content: center; */
-    /* align-items: center; */
-  }
-
+export const StyledDashSideBar = styled.datalist`
   .SideBar {
     width: 300px;
     height: 1203px;
-    background-color: hotpink;
     position: absolute;
+
   }
 
   .SideBarLogo {
@@ -25,11 +15,6 @@ export const StyledDashSideBar = styled.div`
     margin-top: 32px;
   }
   .SideBarContent {
-    /* width: 276px; */
-    /* height: 288px; */
-    /* margin-top: 54px; */
-    /* background-color: black; */
-    /* margin-left: 32px; */
   }
 
   .All-Boards {
@@ -37,109 +22,90 @@ export const StyledDashSideBar = styled.div`
     font-size: 12px;
     font-weight: 700;
     letter-spacing: 2.4;
-    word-wrap: break-word;
+    /* word-wrap: break-word; */
     margin-left: 33px;
     margin-top: 8px;
     width: 126px;
     height: 15px;
-    background-color: white;
   }
 
   .Dashboard {
-    background-color: cadetblue;
-    width: 276px;
+    width: 100%;
     height: 48px;
   }
   .Taskslist {
-    background-color: rosybrown;
-    width: 276px;
-    height: 48px;
+width:100% ;   height: 48px;
   }
   .Teams {
-    background-color: navy;
-    width: 276px;
-    height: 48px;
+width:100%  ; 
+ height: 48px;
   }
   .Calendar {
-    background-color: orangered;
-    width: 276px;
-    height: 48px;
+width:100%  ; 
+ height: 48px;
   }
   .Messages {
-    background-color: tomato;
-    width: 276px;
-    height: 48px;
+width:100%  ;
+height: 48px;
   }
   .Settings {
-    background-color: yellowgreen;
-    width: 276px;
-    height: 48px;
+width:100%  ; 
+ height: 48px;
   }
 
   .MenuItems {
-    color: #828FA3;
+    color: #828fa3;
     font-size: 15;
     font-weight: 700;
   }
 
   .item1 {
-    width: 125px;
+    width: 120px;
     height: 19px;
     align-items: center;
     margin-left: 32px;
-
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    background-color: rebeccapurple;
     padding: 15px;
   }
+
   .item2 {
     width: 104px;
     height: 19px;
     margin-left: 32px;
     align-items: center;
-
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    background-color: rebeccapurple;
     padding: 15px;
   }
   .item3 {
     width: 85px;
     height: 19px;
     align-items: center;
-
     margin-left: 32px;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    background-color: rebeccapurple;
     padding: 15px;
   }
   .item4 {
     width: 105px;
     height: 19px;
     align-items: center;
-
     margin-left: 32px;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    background-color: rebeccapurple;
     padding: 15px;
   }
   .item5 {
-    /* width: 111px;
-    height: 19px; */
     align-items: center;
-
     margin-left: 32px;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    background-color: rebeccapurple;
     padding: 15px;
   }
   .MessageCounter {
@@ -158,7 +124,6 @@ export const StyledDashSideBar = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    background-color: rebeccapurple;
     padding: 15px;
   }
   .logout {
@@ -179,80 +144,130 @@ export const StyledDashSideBar = styled.div`
     width: 251px;
     height: 48px;
     margin: auto;
-    background-color: chartreuse;
     align-items: center;
+    background-color: #f4f7fd;
   }
 
   .ThemeContent {
     width: 105px;
     height: 20px;
-    background-color: brown;
     margin: auto;
     display: flex;
     justify-content: space-evenly;
     margin-top: 48px;
     align-items: center;
     padding: 14px;
+    /* background-color: brown; */
   }
 
   .ThemeToggle-Btn {
     width: 40px;
     height: 20px;
-    background-color: lightsalmon;
     align-items: center;
     display: flex;
+    /* background-color: blueviolet; */
   }
   .SideBarLogo {
     padding: 14px;
   }
 
-  .HideSideBar {
+  /* .HideSideBar {
     width: 276px;
     height: 46px;
-    margin-top: 49px;
-    background-color: goldenrod;
     display: flex;
     align-items: center;
-  }
+  } */
   .HideSideBarText {
-    width: 127px;
+    width: 135px;
     height: 19px;
     margin-left: 31px;
-    background-color: bisque;
     display: flex;
     justify-content: space-between;
     color: #828fa3;
     font-size: 15px;
     font-weight: 700;
+    border: none;
+    margin-top: 49px;
+    background-color: transparent;
+  }
+  
+  .ShowSideBar {
+    width: 56px;
+    height: 48px;
+    border-radius: 0px 100px 100px 0px;
+    background-color: #635fc7;
+    margin-top: 49px;
+    margin-left: 135px;
+  }
+  `;
+
+export const StyledNavContainer = styled.nav`
+width: auto;
+height: 96px;
+display: flex;
+flex-direction: column;
+background-color: salmon;
+`;
+export const LeftContainer = styled.div`
+  flex: 5%;
+  display: flex;
+  align-items: center;
+  padding-left: 1%;
+  background-color: cadetblue;
+
+  .Kanbanlogo {
+    width: 152px;
+  }
+
+  .Navtitle {
+    font-size: 15px;
+    font-weight: 700;
   }
 `;
 
-export const StyledDashSideNav = styled.nav`
-  .NavBar {
-    width: auto;
-    height: 96px;
-    /* background-color: black; */
-    display: flex;
-    flex-direction: row;
-    margin-left: 300px;
-    align-items: center;
-    justify-content: center;
-  }
+export const RightContainer = styled.div`
+  flex: 80%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding-right: 10px;
+  background-color: aquamarine;
 
-  .navtitle{
-    margin-right: 56px;
-    font-size: 24px;
-    font-weight: 700;
-  }
-  .SeacrchBtn-AddNewTask {
+  .SearchBtn-AddNewTask {
     width: 571px;
     height: 48px;
-    /* background-color: white; */
     display: flex;
-    flex-direction: row;
     justify-content: space-between;
     border-radius: 100px;
-    border: 2px solid green;
+    margin-right: 3%;
+  }
+  .SearchBtn {
+    width: 330px;
+    height: 48px;
+    background-color: #e4ebfa;
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    border-radius: 100px;
+    text-indent: 10px;
+  }
+  .SearchBtn input {
+    height: 24px;
+    border: none;
+    align-items: center;
+    background-color: #e4ebfa;
+    width: 80%;
+  }
+  .searchIcon {
+    margin-left: 20px;
+  }
+  input[type="text"]:focus {
+    outline-color: #e4ebfa;
+  }
+  ::placeholder {
+    color: #828fa3;
+    font-size: 15px;
+    font-weight: 700;
   }
 
   .AddNewTask-Btn {
@@ -264,68 +279,70 @@ export const StyledDashSideNav = styled.nav`
     font-size: 15px;
     font-family: 700;
     border: none;
+    align-items: center;
+    font-family: "Plus Jakarta Sans";
+    margin-right: 20px;
     &:active {
       cursor: pointer;
-      transform: translateY(4px);
+      transform: translateY(2px);
     }
     &:hover {
       cursor: pointer;
+      background-color: #a8a4ff;
     }
   }
 
-  .SearchBtn {
-    width: 330px;
-    height: 48px;
-    background-color: #e4ebfa;
-    align-items: center;
-    display: flex;
-    flex-direction: row;
-    border-radius: 100px;
-    text-indent: 10px;
-  }
-
-  .searchIcon {
-    margin-left: 20px;
-  }
-  ::placeholder {
-    color: #828fa3;
-    font-size: 15px;
-    font-weight: 700;
-  }
-
-  .SearchBtn input {
-    width: 84px;
-    height: 24px;
-    /* text-indent: 10px; */
-    border: none;
-    align-items: center;
-    background-color: #e4ebfa;
-    width: 80%;
-  }
-  input[type="text"]:focus {
-    outline-color: #e4ebfa;
-;
-  }
-
   .notification {
-    width: 118px;
-    height: 60px;
-    margin-left: 71px;
+    width: 105px;
     align-items: center;
     display: flex;
     justify-content: space-between;
-    border: 2px solid yellow;
-  }
-  .noti {
-    width: 42px;
-    height: 42px;
   }
   .profile {
     width: 60px;
     height: 60px;
   }
+  .noti {
+    width: 30px;
+    height: 30px;
+  }
 `;
 
+export const NavbarInnerContainer = styled.div`
+  width: auto;
+  height: 96px;
+  display: flex;
+  padding: 10px;
+
+
+  .Navtitle {
+     font-size: 15px;
+    font-weight: 700;
+    align-self: start;
+     align-self: center;
+     margin-right: 30%;
+   }
+`;
+
+
+
+
+
+
+  
+
+
+  
+
+ 
+
+  
+  
+
+  
+
+
+  
 
 
 
