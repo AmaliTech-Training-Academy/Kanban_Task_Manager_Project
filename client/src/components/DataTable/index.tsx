e24264f0ec16360e0b7f9a8edf49987b0916cc2e
Amoatey <emmanuel.amoatey@amalitech.org>
2023-07-25 10:42:23 +0000

    feature/dataTale Branch

---
+++ client/src/components/DataTable/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {useTable} from "react-table";
+
+import * as userData from "../../userData.json"
+
+
+
+
+export const DataTable = () => {
+  console.log(userData)
+  
+
+
+  return (
+    <>
+    </>
+  )
+}
