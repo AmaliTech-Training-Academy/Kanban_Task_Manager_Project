e662b1c745f4fdef8fd3e6e5881e257fc39b674f
Amoatey <emmanuel.amoatey@amalitech.org>
2023-08-02 10:07:50 +0000

    few changes

--- client/src/components/DataTable/styles.ts
+++ client/src/components/DataTable/styles.ts
@@ -8,29 +8,30 @@ export const StyleTable = styled.table`
   margin-top: 1rem;
   position: relative;
   left: 20%;
-  `;
+`;
 
 export const StyleTh = styled.th`
-  background-color: #f2f2f2;
-  height: 52px;
-  padding: 0.5rem 1rem;
-  font-weight: 700;
-  font-size: 12px;
-  color:#3E3F4E;
-  text-align: left;
-  background-color: #e4ebfa;
-  border: none;
+background-color: #f2f2f2;
+height: 52px;
+padding: 0.5rem 1rem;
+font-weight: 700;
+font-size: 12px;
+color:#3E3F4E;
+text-align: left;
+background-color: #e4ebfa;
+border: none;
 `;
 
 export const StyleTd = styled.td`
   padding: 0.5rem 1rem;
   border-bottom: 1px solid #828fa3;
   height: 69px;
-`;
-
-  export const TableContainer = styled.div`
-  
   `;
+
+export const TableContainer = styled.div`
+
+  `
+  ;
 
 
 
