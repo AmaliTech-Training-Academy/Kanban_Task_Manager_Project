d10c9dd16d84a2b0c8ee37393e827849966efaed
Amoatey <emmanuel.amoatey@amalitech.org>
2023-08-05 16:05:02 +0000

    connect registration to backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,11 @@ import * as React from "react";
 import "./index.css";
 import { Admin } from "./admin_registration_page";
 import './App.css'
-import { AdminLogin } from './admin-user-login'
 
 function App() {
   return (
     <>
       <Admin/>
-     <AdminLogin/>
     </>
   );
 }
