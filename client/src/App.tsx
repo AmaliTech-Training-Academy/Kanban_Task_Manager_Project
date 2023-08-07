3b93de1da88f080aab2afda94813a832bf29c0b7
Amoatey <emmanuel.amoatey@amalitech.org>
2023-08-07 20:21:09 +0000

    updated forgot-password apptsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
 import "./index.css";
-import { Admin } from "./admin_registration_page";
 import { ForgotPassword } from './forgot-password'
-import { ResetPassword } from './reset-password'
-import { SetNewPassword } from './set-new-password'
-import { AdminLogin } from './admin-user-login'
+
+
 
 function App() {
   return (
