65733892b6fa0da8f51aae55238aec7a3a2baa8a
Francis aml <francis.osei@amalitech.org>
2023-08-10 12:59:47 +0100

    bugfix: fix jwt malformed bug in authController

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -30,7 +30,7 @@ export const protect = async (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
-    token = req.headers.authorization.split("")[1];
+    token = req.headers.authorization.split(" ")[1];
   } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
   }
