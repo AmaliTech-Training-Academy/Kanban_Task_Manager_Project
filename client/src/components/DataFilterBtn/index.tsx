b817480baca46d5c2aca897518ee7798ae3c66ac
Amoatey <emmanuel.amoatey@amalitech.org>
2023-08-12 18:50:26 +0000

    filterbtn style

--- client/src/components/DataFilterBtn/index.tsx
+++ client/src/components/DataFilterBtn/index.tsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import { StyleFilterBtn } from './styles'
 
+
 export const FilterBtn = () => {
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    (e.target.value)
+  }
+
+
+  
 
 
   return (
     <div>
-        <StyleFilterBtn>
-    
+      <StyleFilterBtn>
+        <select className='FilterBtn' onChange={handleSelect}>
+          <option defaultValue="specialization">
+            {" "}
+            Filters 
+          </option>
+          <label defaultValue="Filters"></label>
 
-        </StyleFilterBtn>
+          <option value="Specialization">Specialization</option>
+          <option value="Rank">Rank</option>
+          <option value="Status">Status</option>
+          <option value="Ascending">Sort Ascending</option>
+          <option value="Descending">Sort Descending</option>
+        </select>
+      </StyleFilterBtn>
     </div>
-  )
+  );
 }
--- client/src/components/DataFilterBtn/styles.ts
+++ client/src/components/DataFilterBtn/styles.ts
@@ -5,7 +5,22 @@ export const StyleFilterBtn = styled.button`
   height: 47px;
   padding: 8px;
   border-radius: 8px;
-  background-color: #828fa3;
-  border: none;
+  border: 1px #828fa3 solid;
+  background-color: transparent;
+
+  .FilterBtn {
+    border: none;
+    background-color: transparent;
+    color: #3e3f4e;
+    font-size: 15px;
+    font-weight: 700;
+  }
+
+  option {
+    font-size: 13px;
+    color: #3e3f4e;
+    text-align: center;
+  }
+
   
 `;
